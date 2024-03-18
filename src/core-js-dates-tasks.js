5b90eb60e8f92abca878cadf5aeec6aadffa20aa
Aleksandr Pervykh <alexander.pervykh.5545@gmail.com>
2024-03-19 02:57:21 +0300

    feat: add getTime

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,9 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const str = date.toString();
+  return str.match(/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/);
 }
 
 /**
