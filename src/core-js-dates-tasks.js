60f064285c172040cd902c6cd78dd6d5e1d00025
Aleksandr Pervykh <alexander.pervykh.5545@gmail.com>
2024-03-20 01:05:37 +0300

    feat: add isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -124,8 +124,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = Date.parse(period.start);
+  const end = Date.parse(period.end);
+  const checkedDate = Date.parse(date);
+  if (start <= checkedDate && checkedDate <= end) return true;
+  return false;
 }
 
 /**
