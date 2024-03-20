7e43816b09b60c6ace21b9655adbcfc0f23583a4
Aleksandr Pervykh <alexander.pervykh.5545@gmail.com>
2024-03-21 00:00:07 +0300

    feat: add getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -216,8 +216,12 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  if (month < 3) return 1;
+  if (month < 6) return 2;
+  if (month < 9) return 3;
+  return 4;
 }
 
 /**
