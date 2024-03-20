72d2998e03b0eed78de9354cdca51e880323f40e
Aleksandr Pervykh <alexander.pervykh.5545@gmail.com>
2024-03-20 20:46:40 +0300

    feat: add formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -143,8 +143,18 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const data = new Date(date.slice(0, 19));
+  const hours = data.getHours() > 12 ? data.getHours() - 12 : data.getHours();
+  const partOfDay = data.getHours() > 11 ? 'PM' : 'AM';
+  const minutes = data.getMinutes();
+  const seconds = data.getSeconds();
+  const month = data.getMonth() + 1;
+  const year = data.getFullYear();
+  const day = data.getDate();
+  return `${month}/${day}/${year}, ${hours}:${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')} ${partOfDay}`;
 }
 
 /**
